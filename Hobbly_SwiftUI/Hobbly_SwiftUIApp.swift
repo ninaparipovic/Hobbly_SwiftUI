640291f549e27f5397b63dfdc063ea6969b29e98
ninaparipovic <nina.paripovic.22@dartmouth.edu>
2021-05-17 11:22:10 -0400

    Initial Commit

---
+++ Hobbly_SwiftUI/Hobbly_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hobbly_SwiftUIApp.swift
+//  Hobbly_SwiftUI
+//
+//  Created by Nina Paripovic on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Hobbly_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
